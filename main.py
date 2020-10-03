d78ceeaf327e48ff2627d7e9bf3dc4f36d70a534
BurlakaR <ro.an.burlaka@gmail.com>
2020-10-03 16:22:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
 import tools
 
+
+def one_page_scrap(parser, url):
+    parser.get(url)
+    tools.waiting_load(parser)
+    question = parser.find_element_by_xpath('//div[contains(@class, "question-problem__describe")]')
+    text = question.text
+    answers = parser.find_elements_by_xpath('//div[contains(@class, "question-answer__content")]]')
+    for answer in answers:
+        text += answer.text
+    print(text)
+    return text
+
+
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://pytania.abczdrowie.pl/pytania-do-specjalistow')
 tools.waiting_load(driver)
@@ -13,6 +26,10 @@ for button in buttons:
 while True:
     questions = driver.find_elements_by_class_name('questions-results__item')
     print(len(questions))
+    links = [question.find_element_by_xpath('.//a').get_attribute('href') for question in questions]
+    print(links)
+    for link in links:
+        one_page_scrap(driver, link)
 
     next = (driver.find_elements_by_class_name('pagination__item'))[-1]
     try:
@@ -21,5 +38,6 @@ while True:
         break
     driver.get(next_link.get_attribute('href'))
     tools.waiting_load(driver)
+
 
 
