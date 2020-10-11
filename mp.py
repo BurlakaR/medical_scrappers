311f1d889f74e1c73fc2d5b0c3a9129b08598298
BurlakaR <ro.an.burlaka@gmail.com>
2020-10-11 16:52:59 +0200

    add_mp_website

---
+++ mp.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+import tools
+
+
+def scrap_one_list(list_parser, list):
+    list_parser.get(list)
+    tools.waiting_load(list_parser)
+    questions = []
+    while(True):
+        try:
+            questions.extend([link.get_attribute('href') for link in list_parser.find_elements_by_xpath('.//li/div/h2/a')])
+            next = list_parser.find_element_by_xpath('.//li[contains(@class, "next-page")]/a').get_attribute('href')
+            list_parser.get(next)
+        except:
+            break
+    return questions
+
+def scrap_one_page(page_parser, page):
+    page_parser.get(page)
+    text_div = page_parser.find_element_by_xpath('.//div[contains(@class, "article-show-content")]')
+    return text_div.text
+
+
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--window-size=1920,1080')
+driver = webdriver.Chrome('chromedriver', chrome_options=options)
+driver.get('https://www.mp.pl/pacjent/')
+tools.waiting_load(driver)
+
+links = driver.find_elements_by_xpath('.//ul[contains(@class, "list-unstyled")]/li/a')
+links_to_scrap = [link.get_attribute('href') + 'lista' for link in links
+                  if 'pacjent' in link.get_attribute('href') and 'leki' not in link.get_attribute('href')]
+for list in links_to_scrap:
+    questions = scrap_one_list(driver, list)
+    for q in questions:
+        text = scrap_one_page(driver, q)
+        name = q[q.rindex('/') + 1:]
+        name = str(name).replace('?', '').replace('=', '')
+        with open('texts/' + name + '.txt', 'w', encoding='utf-8') as text_file:
+            text_file.write(text)
+
+
+
